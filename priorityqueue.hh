418c2ddf20221e873c8f188fe4a1e9be9878460b
Artur Myszkowski <am347189@students.mimuw.edu.pl>
2015-12-30 23:11:38 +0100

    priorityqueue.hh added

---
+++ priorityqueue.hh
@@ -0,0 +1,14 @@
+#ifndef __PRIORITYQUEUE_HH__
+#define __PRIORITYQUEUE_HH__
+
+template<typename K, typename V>
+class PriorityQueue {
+
+public:
+
+private:
+
+}
+
+#endif /* __PRIORITYQUEUE_HH__ */
+
