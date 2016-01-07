2e075b7eb9b98b875da8a25d018159f096e1b838
Artur Myszkowski <am347189@students.mimuw.edu.pl>
2016-01-07 20:58:02 +0100

    komentarze

--- priorityqueue.hh
+++ priorityqueue.hh
@@ -3,6 +3,13 @@
 /*                        Katarzyna Herba  - kh359525                         */
 /*                        Artur Myszkowski - am347189                         */
 /*============================================================================*/
+/* Implementacja kolejki priorytetowej opiera się na strukturach              */
+/* przechowujących smart pointer'y - shared_ptr. Każda para (Key, Value) jest */
+/* tworzona raz, a opearcje wykonywane są na wskaźnikach. Użyte kontenery to  */
+/* map<shared_ptr<K>, multiset<shared_ptr<V>>> oraz                           */
+/* map<shared_ptr<V>, multiset<shared_ptr<K>>> z odpowiednio zdefiniowanym    */
+/* komparatorem.                                                              */
+/*============================================================================*/
 
 #ifndef __PRIORITYQUEUE_HH__
 #define __PRIORITYQUEUE_HH__
@@ -11,16 +18,13 @@
 #include <map>
 #include <set>
 #include <algorithm>
-#include <cassert> //
-#include <iostream> //
-
+#include <cassert>
+#include <utility>
 
 /*============================================================================*/
 /*                                 Wyjątki.                                   */
 /*============================================================================*/
 
-
-// TODO noexcept zamiast throw() ?
 class PriorityQueueEmptyException: public std::exception {
 
 public:
@@ -39,26 +43,9 @@ public:
    }
 };
 
-
 /*============================================================================*/
 /*                                Interfejs.                                  */
 /*============================================================================*/
-
-
-// Predeklaracje operatorów dla oddzielenia interfejsu od implementacji.
-template<typename K, typename V>
-class PriorityQueue;
-
-template<typename K, typename V>
-bool operator==(const PriorityQueue<K, V>& lhs, 
-                const PriorityQueue<K, V>& rhs);
-
-template<typename K, typename V>
-bool operator<(const PriorityQueue<K, V>& lhs, 
-               const PriorityQueue<K, V>& rhs);
-
-//template<typename K, typename V>
-//void swap(PriorityQueue<K, V>& queue1, PriorityQueue<K, V>& queue2);
 
 template<typename K, typename V>
 class PriorityQueue {
@@ -70,20 +57,17 @@ public:
    using value_type = V;
 
    /**
-    * Konstruktor bezparametrowy tworzący pustą kolejkę.
-    * [O(1)]
+    * Konstruktor bezparametrowy tworzący pustą kolejkę. [O(1)]
     */
    PriorityQueue() {}
 
    /**
-    * Konstruktor kopiujący. 
-    * [O(queue.size())]
+    * Konstruktor kopiujący. [O(queue.size())]
     */
    PriorityQueue(const PriorityQueue<K, V>& queue);
 
    /**
-    * Konstruktor przenoszący. 
-    * [O(1)]
+    * Konstruktor przenoszący. [O(1)]
     */
    PriorityQueue(PriorityQueue<K, V>&& queue);
 
@@ -94,8 +78,7 @@ public:
    PriorityQueue<K, V>& operator=(PriorityQueue<K, V> queue);
 
    /**
-    * Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta. 
-    * [O(1)]
+    * Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta. [O(1)]
     */
    bool empty() const;
 
@@ -108,7 +91,7 @@ public:
    /**
     * Metoda wstawiająca do kolejki parę o kluczu key i wartości value
     * [O(log size())] (dopuszczamy możliwość występowania w kolejce wielu
-    * par o tym samym kluczu)
+    * par o tym samym kluczu).
     */
    void insert(const K& key, const V& value);
 
@@ -116,7 +99,7 @@ public:
     * Metody zwracające odpowiednio najmniejszą i największą wartość 
     * przechowywaną w kolejce [O(1)]; w przypadku wywołania którejś z tych metod
     * na pustej strukturze powinien zostać zgłoszony wyjątek 
-    * PriorityQueueEmptyException
+    * PriorityQueueEmptyException.
     */
    const V& minValue() const;
    
@@ -126,7 +109,7 @@ public:
     * Metody zwracające klucz o przypisanej odpowiednio najmniejszej lub
     * największej wartości [O(1)]; w przypadku wywołania którejś z tych metod
     * na pustej strukturze powinien zostać zgłoszony wyjątek
-    * PriorityQueueEmptyException
+    * PriorityQueueEmptyException.
     */
    const K& minKey() const;
 
@@ -134,8 +117,7 @@ public:
 
    /**
     * Metody usuwające z kolejki jedną parę o odpowiednio najmniejszej lub
-    * największej wartości. 
-    * [O(log size())]
+    * największej wartości. [O(log size())]
     */
    void deleteMin();
    
@@ -147,24 +129,23 @@ public:
     * o kluczu key, powinien zostać zgłoszony wyjątek
     * PriorityQueueNotFoundException(); w przypadku kiedy w kolejce jest kilka 
     * par o kluczu key, zmienia wartość w dowolnie wybranej parze o podanym 
-    * kluczu
+    * kluczu.
     */
    void changeValue(const K& key, const V& value);
 
    /**
     * Metoda scalająca zawartość kolejki z podaną kolejką queue; ta operacja 
-    * usuwa wszystkie elementy z kolejki queue i wstawia je do kolejki *this
+    * usuwa wszystkie elementy z kolejki queue i wstawia je do kolejki *this.
     * [O(size() + queue.size() * log (queue.size() + size()))]
     */
    void merge(PriorityQueue<K, V>& queue);
 
    /**
     * Metoda zamieniającą zawartość kolejki z podaną kolejką queue (tak jak
-    * większość kontenerów w bibliotece standardowej). 
-    * [O(1)]
+    * większość kontenerów w bibliotece standardowej). [O(1)]
     */
    void swap(PriorityQueue<K, V>& queue);
-
+  
    bool operator==(const PriorityQueue<K, V>& queue) const;
    
    bool operator<(const PriorityQueue<K, V>& queue) const;
@@ -229,17 +210,6 @@ public:
       }
    }
 
-   /** 
-    * Globalna metoda swap. [O(1)]
-    */
-   // TODO na razie tu
-   friend void swap(PriorityQueue<K, V>& queue1, PriorityQueue<K, V>& queue2) {
-      using std::swap;
-      std::swap(queue1.map_key, queue2.map_key);
-      std::swap(queue1.map_value, queue2.map_value);
-      std::swap(queue1.counter, queue2.counter);
-   }
-
 private:
 
    template<typename T>
@@ -261,17 +231,14 @@ private:
    size_type counter = 0;
 };
 
-
 /*============================================================================*/
 /*                             Implementacja.                                 */
 /*============================================================================*/
 
-
 template<typename K, typename V>
 PriorityQueue<K, V>::PriorityQueue(const PriorityQueue<K, V>& queue) {
-// TODO czy blok try catch jest potrzebny i czy wgl tak moze byc ?
-   map_key_t tmp_key(queue.map_key);
-   map_value_t tmp_value(queue.map_value);
+   map_key_t tmp_key(queue.map_key); // O(queue.size())
+   map_value_t tmp_value(queue.map_value); // O(queue.size()
    map_key.swap(tmp_key);
    map_value.swap(tmp_value);
    counter = queue.counter; 
@@ -284,7 +251,6 @@ PriorityQueue<K, V>::PriorityQueue(PriorityQueue<K, V>&& queue) {
    counter = queue.counter; 
 }
 
-// Unifying assigment operator ma silną gwarancję.
 template<typename K, typename V>
 PriorityQueue<K, V>& PriorityQueue<K, V>::operator=(PriorityQueue<K, V> queue) {
    queue.swap(*this);
@@ -305,7 +271,7 @@ template<typename K, typename V>
 void PriorityQueue<K, V>::insert(const K& key, const V& value) {
    ptr_key_t tmp_k = std::make_shared<K>(key);
    ptr_value_t tmp_v = std::make_shared<V>(value);
-   auto it = map_key.find(tmp_k);
+   auto it = map_key.find(tmp_k); // O(log size()))
    typename set_value_t::const_iterator set_it;
    bool first = false;
    if (it == map_key.end()) {
@@ -315,53 +281,43 @@ void PriorityQueue<K, V>::insert(const K& key, const V& value) {
       auto it_tmp = map_key.insert(std::make_pair(tmp_k,s));
       it = it_tmp.first;
    } else {
-      set_it = map_key[tmp_k].insert(tmp_v);
+      set_it = map_key[tmp_k].insert(tmp_v); // O(log size()))
    }
-   // TODO: (@artur) ogarnij, czy tak jest dobrze -
-   // mi sie po wielu bolach wydaje, ze jest
    try {
-      //tmp_k = std::make_shared<K>(key);
-      //tmp_v = std::make_shared<V>(value);
       map_value[tmp_v].insert(tmp_k);
    } catch (...) {
       if (first) {    
-         map_key.erase(it);
+         map_key.erase(it); // O(1)
       } else {
-         it->second.erase(set_it);
+         it->second.erase(set_it); // O(1)
       }
       throw;
    }
-/*
-//TEST
-std::cout << "key: " << map_key.size() << " " << map_key.begin()->second.size() << std::endl;
-std::cout << "value: " <<map_value.size() << " " << map_value.begin()->second.size() << std::endl;
-std::cout << "-------------------------------------------------------\n";
-*/
    ++counter;
 }
 
-template<typename K, typename V> // K?
+template<typename K, typename V>
 const V& PriorityQueue<K, V>::minValue() const {
    if (empty())
       throw PriorityQueueEmptyException();
    return *map_value.cbegin()->first;
 }
 
-template<typename K, typename V> // K?
+template<typename K, typename V>
 const V& PriorityQueue<K, V>::maxValue() const {
    if (empty())
       throw PriorityQueueEmptyException();
    return *map_value.crbegin()->first;
 }
 
-template<typename K, typename V> // V?
+template<typename K, typename V>
 const K& PriorityQueue<K, V>::minKey() const {
    if (empty())
       throw PriorityQueueEmptyException();
    return **map_value.cbegin()->second.begin();
 }
 
-template<typename K, typename V> // V?
+template<typename K, typename V>
 const K& PriorityQueue<K, V>::maxKey() const {
    if (empty())
       throw PriorityQueueEmptyException();
@@ -372,21 +328,14 @@ template<typename K, typename V>
 void PriorityQueue<K, V>::deleteMin() {
     if (empty())
       return;
-/*
-//TEST
-std::cout << "key: " << map_key.size() << " " << map_key.begin()->second.size() << std::endl;
-std::cout << "value: " <<map_value.size() << " " << map_value.begin()->second.size() << std::endl;
-std::cout << "-------------------------------------------------------\n";
-*/
-
 
    auto it_k = map_value.cbegin()->second.cbegin();
    ptr_key_t tmp_k = *it_k;
    ptr_value_t tmp_v = map_value.cbegin()->first;
 
-   auto it_map_key = map_key.find(tmp_k);
+   auto it_map_key = map_key.find(tmp_k); // O(log size()))
    assert(it_map_key != map_key.end());
-   auto it_map_key_set = it_map_key->second.find(tmp_v);
+   auto it_map_key_set = it_map_key->second.find(tmp_v); // O(log size()))
    assert(it_map_key_set != it_map_key->second.end());
 
    // Usunięcie klucza spod min wartości.
@@ -395,9 +344,9 @@ std::cout << "-------------------------------------------------------\n";
       map_value.erase(map_value.cbegin()); // O(1)
 
    // Usunięcie tej wartości spod klucza.
-   it_map_key->second.erase(it_map_key_set);
+   it_map_key->second.erase(it_map_key_set); // O(1)
    if (it_map_key->second.empty())
-      map_key.erase(it_map_key);
+      map_key.erase(it_map_key); // O(1)
  
    --counter;
 }
@@ -411,9 +360,9 @@ void PriorityQueue<K, V>::deleteMax() {
    ptr_key_t tmp_k = *it_k;
    ptr_value_t tmp_v = map_value.crbegin()->first;
 
-   auto it_map_key = map_key.find(tmp_k);
+   auto it_map_key = map_key.find(tmp_k); // O(log size()))
    assert(it_map_key != map_key.end());
-   auto it_map_key_set = it_map_key->second.find(tmp_v);
+   auto it_map_key_set = it_map_key->second.find(tmp_v); // O(log size()))
    assert(it_map_key_set != it_map_key->second.end());
 
    // Usunięcie klucza spod max wartości.
@@ -422,9 +371,9 @@ void PriorityQueue<K, V>::deleteMax() {
       map_value.erase(--map_value.crbegin().base()); // O(1)
 
    // Usunięcie tej wartości spod klucza.
-   it_map_key->second.erase(it_map_key_set);
+   it_map_key->second.erase(it_map_key_set); // O(1)
    if (it_map_key->second.empty())
-      map_key.erase(it_map_key);
+      map_key.erase(it_map_key); // O(1)
  
    --counter;
 }
@@ -432,20 +381,21 @@ void PriorityQueue<K, V>::deleteMax() {
 template<typename K, typename V>
 void PriorityQueue<K, V>::changeValue(const K& key, const V& value) { 
    ptr_key_t tmp_k = std::make_shared<K>(key);
+
    // Znajdowanie klucza.
-   auto it_k = map_key.find(tmp_k); // log(size)
+   auto it_k = map_key.find(tmp_k); // O(log size())
    if (it_k == map_key.end())
       throw PriorityQueueNotFoundException();
+
    // Uzyskanie iteratorow do elementow, ktore będą usuwane
    // przy pomyślnym insercie nowego elementu.
    ptr_value_t tmp_v = *it_k->second.begin();
-   auto map_value_it = map_value.find(tmp_v);
+   auto map_value_it = map_value.find(tmp_v); // O(log size())
    auto set_key_it = it_k->second.begin();
    typename set_key_t::const_iterator set_it;
    bool single = (map_value_it->second.size() == 1) ? true : false;
-   if (!single) {
-      set_it = map_value_it->second.find(tmp_k);
-   }
+   if (!single)
+      set_it = map_value_it->second.find(tmp_k); // O(log size())
    
    insert(key, value);
    --counter;
@@ -453,12 +403,11 @@ void PriorityQueue<K, V>::changeValue(const K& key, const V& value) {
    // Usunięcie elementów przy zakończonym sukcesem insercie,
    // ponieważ begin() oraz erase() dla iteratorów są
    // no-throw, tu wyjątek nie może się pojawić.
-   it_k->second.erase(set_key_it);
-   if (single) {
-      map_value.erase(map_value_it);
-   } else {
-      map_value_it->second.erase(set_it);
-   }
+   it_k->second.erase(set_key_it); // O(1)
+   if (single)
+      map_value.erase(map_value_it); // O(1)
+   else
+      map_value_it->second.erase(set_it); // O(1)
 }
 
 template<typename K, typename V>
@@ -467,22 +416,22 @@ void PriorityQueue<K, V>::merge(PriorityQueue<K, V>& queue) {
    if (this == &queue)
       return;
 
-   // Merge *this z queue.
+   // Merge z queue.
    PriorityQueue<K, V> tmp(*this);
 
+   // Obie pętle O(queue.size() * log (queue.size() + size()))
    for (auto it = queue.map_key.begin(); it != queue.map_key.end(); ++it)
       tmp.map_key[it->first].insert(it->second.begin(), it->second.end());
 
    for (auto it = queue.map_value.begin(); it != queue.map_value.end(); ++it)
       tmp.map_value[it->first].insert(it->second.begin(), it->second.end());
 
-   //tmp.map_key.insert(queue.map_key.begin(), queue.map_key.end()); 
-   //tmp.map_value.insert(queue.map_value.begin(), queue.map_value.end()); 
    tmp.counter += queue.counter;
    tmp.swap(*this);
+
    // Czyszczenie queue. clear() jest no-throw.
-   queue.map_key.clear();
-   queue.map_value.clear();
+   queue.map_key.clear(); // O(queue.size())
+   queue.map_value.clear(); //  O(queue.size())
    queue.counter = 0;
 }
 
@@ -491,6 +440,12 @@ void PriorityQueue<K, V>::swap(PriorityQueue<K, V>& queue) {
    std::swap(map_key, queue.map_key);
    std::swap(map_value, queue.map_value);
    std::swap(counter, queue.counter);
+}
+
+// Globalna metoda swap.
+template<typename K, typename V>
+void swap(PriorityQueue<K, V>& lhs, PriorityQueue<K, V>& rhs) {
+   lhs.swap(rhs);
 }
 
 template<typename K, typename V>
@@ -528,6 +483,11 @@ bool PriorityQueue<K, V>::operator==(const PriorityQueue<K, V>& queue) const {
 }
 
 template<typename K, typename V>
+bool PriorityQueue<K, V>::operator!=(const PriorityQueue<K, V>& queue) const {
+   return !(*this == queue);
+}
+
+template<typename K, typename V>
 bool PriorityQueue<K, V>::operator<(const PriorityQueue<K, V>& queue) const {
    auto lhs_it = map_key.begin();
    auto rhs_it = queue.map_key.begin();
@@ -554,11 +514,6 @@ bool PriorityQueue<K, V>::operator<(const PriorityQueue<K, V>& queue) const {
       ++rhs_it;
    }
    return (lhs_it == map_key.end() && rhs_it != queue.map_key.end());
-}
-
-template<typename K, typename V>
-bool PriorityQueue<K, V>::operator!=(const PriorityQueue<K, V>& queue) const {
-   return !(*this == queue);
 }
 
 template<typename K, typename V>
